2b94685653cb4e2201f204d7328f949afd91e006
erick0509 <erinjiva501@gmail.com>
2022-08-29 20:01:32 +0200

    tester la nouvelle branche

--- nouvelle_commande.php
+++ nouvelle_commande.php
@@ -48,25 +48,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <legend>Ajouter une ligne de commande pour ce client</legend>
-                                </div>
-                                <div class="row">
-                                    <div class="form-group">
-                                        <label class="col-md-1">article: </label>
-                                        <div class="col-md-2">
-                                            <input type="text" name="article" class="form-control input-sm" >
-                                        </div>
-                                        <label class="col-md-2">quantite commande: </label>
-                                        <div class="col-md-2">
-                                            <input type="text" name="quantite_commande" class="form-control input-sm">
-                                        </div>
-                                        <label class="col-md-2">quantite livre: </label>
-                                        <div class="col-md-2">
-                                            <input type="text" name="quantite_livre" class="form-control input-sm">
-                                        </div>
-                                    </div>
-                                </div>     
+                               
                             </form>
                         </div>
                     </aside>
