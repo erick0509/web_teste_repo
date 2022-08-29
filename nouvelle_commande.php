46fbe8c0b2520b3dfa59b69be2f4fcfa45a670e2
erick0509 <erinjiva501@gmail.com>
2022-08-29 19:34:43 +0200

    modification des fichier nouvelle_commande.php

--- nouvelle_commande.php
+++ nouvelle_commande.php
@@ -17,37 +17,53 @@
                         <div class="row">
                             <form class=" col-md-12" method="POST" action="save_commande.php">
                                 <div class="form-group">
-                                    <legend>Passer une nouvelle commande</legend>
+                                    <legend>Passer une nouvelle commande d'un client</legend>
                                 </div>
                                 <div class="row">
                                     <div class="form-group">
                                         <label class="col-md-1">client: </label>
                                         <div class="col-md-2">
-                                            <input type="text" name="nom" class="form-control input-sm" >
+                                            <select style="font-size:8px;" class="form-control" name='client'>
+                                                <?php
+                                                    $dom = new DOMDocument('1.0', 'utf-8');
+                                                    $xml = file_get_contents('client.xml');
+                                                    @$dom->loadHTML($xml);
+                                                    $a = $dom->getElementsByTagName('client');
+                                                    $nom=$dom->getElementsByTagName('nom');
+                                                    for ($i=0; $i < $a->length; $i++) {
+                                                        $attr = $a->item($i)->getAttribute('id');
+                                                        $attrnom=$nom->item($i)->nodeValue;
+                                                        ?><option> <?php echo $attr.")".$attrnom?> </option> <?php 
+                                                    }
+                                                ?>
+                                            </select>
                                         </div>
                                         <label class="col-md-2">date de commande: </label>
                                         <div class="col-md-2">
-                                            <input type="date" name="adresse" class="form-control input-sm">
+                                            <input type="date" name="date_commande" class="form-control input-sm">
                                         </div>
                                         <label class="col-md-2">date de livraison: </label>
                                         <div class="col-md-2">
-                                            <input type="date" name="age" class="form-control input-sm">
+                                            <input type="date" name="date_livraison" class="form-control input-sm">
                                         </div>
                                     </div>
+                                </div>
+                                <div class="form-group">
+                                    <legend>Ajouter une ligne de commande pour ce client</legend>
                                 </div>
                                 <div class="row">
                                     <div class="form-group">
                                         <label class="col-md-1">article: </label>
                                         <div class="col-md-2">
-                                            <input type="text" name="nom" class="form-control input-sm" >
+                                            <input type="text" name="article" class="form-control input-sm" >
                                         </div>
-                                        <label class="col-md-2">date de commande: </label>
+                                        <label class="col-md-2">quantite commande: </label>
                                         <div class="col-md-2">
-                                            <input type="date" name="adresse" class="form-control input-sm">
+                                            <input type="text" name="quantite_commande" class="form-control input-sm">
                                         </div>
-                                        <label class="col-md-2">date de livraison: </label>
+                                        <label class="col-md-2">quantite livre: </label>
                                         <div class="col-md-2">
-                                            <input type="date" name="age" class="form-control input-sm">
+                                            <input type="text" name="quantite_livre" class="form-control input-sm">
                                         </div>
                                     </div>
                                 </div>     
